22c31167a066cb0dea6da9f5123d2204aa23cb45
daichikuwa0618 <31601805+daichikuwa0618@users.noreply.github.com>
2017-10-04 16:47:48 +0900

    main の記述の追加

--- handSensor.py
+++ handSensor.py
@@ -16,6 +16,7 @@ from time import sleep
 adc0 = MCP3208(channel = 0)
 adc1 = MCP3208(channel = 1)
 adc2 = MCP3208(channel = 2)
+
 pwm = pigpio.pi()
 
 # ========== constants ==========
@@ -28,15 +29,15 @@ GET_OBJECT   = 0.8
 NEAR_OBJECT  = 0.5
 CLOSE_OBJECT = 0.2
 TOUCH_OBJECT = 0.1
-# speed of servoMotor
-MAX_SPEED = 2.0
-MID_SPEED = 1.0
-MIN_SPEED = 0.5
+# speed of servoMotor[deg]
+MAX_DEG = 2.0
+MID_DEG = 1.0
+MIN_DEG = 0.5
 
 # ========== variables ==========
 command    = 0 # 0:OPEN, 1:CLOSE
 stateFing1 = 0 # distance between fing to object
-stateFing2 = 0 # state assignments 0:far 1:mid 2:close
+stateFing2 = 0 # state assignments
 stateFing3 = 0 # 0:far 1:mid 2:close
 
 degree1    = 0.0 # servo angle for each servoMotors
@@ -49,7 +50,6 @@ def moveServo(pin, degree):
     if degree <= 60.0 and degree >= -60.0:
         usec = 1500.0 + (degree * 10.0)
         pi.set_servo_pulsewidth(pin, usec)
-        sleep(0.1)
 
 # ========== Function commanding servoMotor ==========
 def cmdServo(adcChannel, degree, state):
@@ -61,24 +61,24 @@ def cmdServo(adcChannel, degree, state):
         # change state and show message in first change
         if state not 3:
             state = 3
-            print("Finger" + str(adcChannel + 1) + "has been changed to state1")
+            print("Finger" + str(adcChannel + 1) + "has been changed to state3")
     # close to object
     elif adcList[adcChannel] <= CLOSE_OBJECT:
-        degree -= MIN_SPEED
+        degree -= MIN_DEG
         # change state and show message in first change
         if state not 2:
             state = 2
-            print("Finger" + str(adcChannel + 1) + "has been changed to state1")
+            print("Finger" + str(adcChannel + 1) + "has been changed to state2")
     # mid distance to object
     elif adcList[adcChannel] <= NEAR_OBJECT:
-        degree -= MID_SPEED
+        degree -= MID_DEG
         # change state and show message in first change
         if state not 1:
             state = 1
             print("Finger" + str(adcChannel + 1) + "has been changed to state1")
     # far from object
     else:
-        degree -= MAX_SPEED
+        degree -= MAX_DEG
         state = 0
 
     return degree, state
@@ -91,40 +91,41 @@ moveServo(FING3_PIN, 0)
 moveServo(JOINT_PIN, 0)
 
 # ========== main ==========
-try:
-    # Loop for infinite
-    while True:
-        # command input
-        command = int(input("Type '0':OPEN, '1':CLOSE ...:"))
+if __name__ == '__main__':
+    try:
+        # Loop for infinite
+        while True:
+            # command input
+            command = int(input("Type '0':OPEN, '1':CLOSE ...:"))
 
-        # OPEN command
-        if command == 0:
-            print("opening fingers")
-            ''' Need speed control method in this area too '''
-            moveServo(FING1_PIN, 0.0)
-            moveServo(FING2_PIN, 0.0)
-            moveServo(FING3_PIN, 0.0)
+            # OPEN command
+            if command == 0:
+                print("opening fingers")
+                ''' Need speed control method in this area too '''
+                moveServo(FING1_PIN, 0.0)
+                moveServo(FING2_PIN, 0.0)
+                moveServo(FING3_PIN, 0.0)
 
-        # CLOSE command
-        elif command == 1:
-            print("closing fingers")
-            # until ALL fingers touch to object
-            while (stateFing1 not 3) or (stateFing2 not 3) or (stateFing3 not 3):
-                # USAGE: cmdServo(channel of ADC, degree, state)
-                degree1, stateFing1 = cmdServo(0, degree1, stateFing1)
-                degree2, stateFing2 = cmdServo(1, degree2, stateFing2)
-                degree3, stateFing3 = cmdServo(2, degree3, stateFing3)
-                # move servoMotors according to angle value of cmdServo
-                moveServo(FING1_PIN, degree1)
-                moveServo(FING2_PIN, degree2)
-                moveServo(FING3_PIN, degree3)
+            # CLOSE command
+            elif command == 1:
+                print("closing fingers")
+                # until ALL fingers touch to object
+                while (stateFing1 not 3) or (stateFing2 not 3) or (stateFing3 not 3):
+                    # USAGE: cmdServo(channel of ADC, degree, state)
+                    degree1, stateFing1 = cmdServo(0, degree1, stateFing1)
+                    degree2, stateFing2 = cmdServo(1, degree2, stateFing2)
+                    degree3, stateFing3 = cmdServo(2, degree3, stateFing3)
+                    # move servoMotors according to angle value of cmdServo
+                    moveServo(FING1_PIN, degree1)
+                    moveServo(FING2_PIN, degree2)
+                    moveServo(FING3_PIN, degree3)
 
-        # Typed wrong command
-        else:
-            print("Error command typed...")
+            # Typed wrong command
+            else:
+                print("Error command typed...")
 
-except KeyboardInterrupt:
-    pass
+    except KeyboardInterrupt:
+        pass
 
 # ========== destroy ==========
 # force to stop PWM by handing '0' to second arguement
